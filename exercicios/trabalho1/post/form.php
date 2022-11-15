9a80d77fd3250b6301bf20b766a7ae5bb1311f06
Luiz <luizfranzon05@hotmail.com>
2022-11-15 19:17:24 -0300

    trabalho POST GET

---
+++ exercicios/trabalho1/post/form.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Informações recebidas</title>
+
+  <style>
+
+  * {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 2.5rem;
+    background-color: #0377fc;
+    border-radius: 8px;
+    padding: 8px;
+  }
+
+  span {
+    font-size: 1.5rem;
+  }
+
+  </style>
+
+</head>
+<body>
+  <h1>Informações recebidas por POST</h1>
+  <?php
+
+  echo "<div>";
+  echo "<span><strong>Nome</strong>: " . $_POST["nome"] . "</span><br>";
+  echo "<span><strong>Idade</strong>: " . $_POST["idade"] . "</span><br>";
+  echo "<span><strong>Rua</strong>: " . $_POST["rua"] . "</span><br>";
+  echo "<span><strong>Cidade</strong>: " . $_POST["cidade"] . "</span><br>";
+  echo "</div>";
+
+  ?>
+</body>
+</html>
